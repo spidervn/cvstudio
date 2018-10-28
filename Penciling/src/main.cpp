a7a50e35080117c9a89ccf541ae299e2c10ad61c
truyenspidermanvn <a@vnn.vn>
2018-10-28 07:43:56 +0700

    Bilateral filter

--- Penciling/src/main.cpp
+++ Penciling/src/main.cpp
@@ -17,16 +17,19 @@ int main(int argc, char const *argv[])
     cv::Mat abs_grad_y;
     cv::Mat grad;
     cv::Mat invertgrad;
+    cv::Mat img_Bil;
     int ksize = -1; // parser.get<int>("ksize");
     int scale = 1; //parser.get<int>("scale");
     int delta = 0; // parser.get<int>("delta");
     int ddepth = CV_16S;
 
     const char* szImageToPencil = "Image2Pencil";
+    const char* szBilateral = "Bilateral";
 
     img = imread(argv[1], IMREAD_COLOR);
 
     GaussianBlur(img, img_NoNoise, Size(3,3), 0, 0, BORDER_DEFAULT);
+    bilateralFilter(img, img_Bil, 30, 60, 60);
     cvtColor(img_NoNoise, imgGray, COLOR_BGR2GRAY);
     
     Sobel(imgGray, grad_x, ddepth, 1, 0, ksize, scale, delta, BORDER_DEFAULT);
@@ -38,10 +41,13 @@ int main(int argc, char const *argv[])
 
     invertgrad = (255-grad);
     namedWindow(szImageToPencil, WINDOW_AUTOSIZE);
+    namedWindow(szBilateral, WINDOW_AUTOSIZE);
+
     imshow(szImageToPencil, invertgrad);
+    imshow(szBilateral, img_Bil);
 
     imwrite("./out.jpg", invertgrad);
-
+    imwrite("./out2.jpg", img_Bil);
     waitKey(0);
     return 0;
 }
