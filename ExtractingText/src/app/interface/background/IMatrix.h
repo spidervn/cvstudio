e382155c7a291127b7c61b3666a26f3a0fa3535e
ducvd <truyenspidermanvn@gmail.com>
2019-01-09 22:37:41 +0700

    Add Matrix Libraries

--- ExtractingText/src/app/interface/background/IMatrix.h
+++ ExtractingText/src/app/interface/background/IMatrix.h
@@ -2,6 +2,55 @@
 #define EXTRACTTEXT_APP_INTERFACE_BACKGROUND_IMATRIX_H_
 
 #include <vector>
+#include <stdio.h>   
+#include <stdlib.h> 
+
+template <typename T>
+class Matrix
+{
+public:
+    Matrix(int rows, int cols)
+    {
+        m = rows;
+        n = cols;
+
+        // Dummiest 
+        _p_Data = malloc(m*n*sizeof(T));
+    }
+
+    ~Matrix()
+    {
+        __freeMatrix();
+    }
+
+    int Rebuild(int rows, int cols)
+    {
+        __freeMatrix();
+
+        m = rows;
+        n = cols;
+
+        // Dummiest 
+        _p_Data = (T*)malloc(m*n*sizeof(T));
+
+    }
+   
+private:    
+
+    int __freeMatrix()
+    {
+        if (_p_Data)
+        {
+            free(_p_Data);
+            _p_Data = NULL;
+        }
+    }
+
+    int m,n;
+
+    T* _p_Data;
+
+};
 
 class IMatrix
 {
@@ -11,6 +60,8 @@ public:
     virtual double dotProduct(std::vector<double> p1, 
                                 std::vector<double> p2,
                                 int n) = 0;
+
+    virtual double MatrixProduct() = 0;
 };
 
 #endif
