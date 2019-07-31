4e3b09dd034db2647161948bdba461213bef29f7
ducvd <ducvd@aimesoft.com>
2019-07-31 10:50:41 +0700

    Apply distance transform

--- ExtractingText/src/app/impl/application/CExtractTextApp.cpp
+++ ExtractingText/src/app/impl/application/CExtractTextApp.cpp
@@ -21,28 +21,38 @@ int CExtractTextApp::run(int argc, char const *argv[])
     // Load images
     //  1- Use thresholding to removing background
     //      Threshold to Zero
-    //  2- 
+    //  2- Then extract every characters 
+    //  3- Apply image segmentations
+
     
 
     cv::Mat img;
     cv::Mat img_gray;
     cv::Mat img_inv;
     cv::Mat dst;
+    cv::Mat df;
     //1) Read
     img = imread(argv[1], IMREAD_COLOR);
     cvtColor(img, img_gray, COLOR_BGR2GRAY);
     img_inv = 255 - img_gray;
 
     threshold(img_inv, dst, 190, 255, THRESH_TOZERO);
-
+    distanceTransform(dst, df, DIST_L2, 3);
+    // 
+    // Invert color 
+    // Thresholding? 
+    // 
     const char* szInvert = "Invert color";
     const char* szThreshold = "Thresholding";
+    const char* szDF = "Distance Tranform";
 
     namedWindow(szInvert,  WINDOW_AUTOSIZE);
     namedWindow(szThreshold,  WINDOW_AUTOSIZE);
+    namedWindow(szDF, WINDOW_AUTOSIZE);
     
     imshow(szInvert, img_inv);
     imshow(szThreshold, dst);
+    imshow(szDF, df);
 
     waitKey();
     return 0;
